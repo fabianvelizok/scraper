3216e8574197f6d54adbd4e347189959f4063416
Fabian Veliz <velizfabianhoracio@gmail.com>
2025-08-19 15:22:12 -0300

    Complete Solid setup with single database

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Scraper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.solid_cache.connects_to = nil
+    config.solid_queue.connects_to = nil
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
